6a88919dfa58b771b924f1ee3689794b9775bd6d
manohar108 <chmanohar93@gmail.com>
2017-11-24 19:34:28 +0530

    example

---
+++ src/main/java/fish/example.java
@@ -0,0 +1,12 @@
+package fish;
+
+public class example {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("new test");
+		System.out.println("second line 123455");
+
+	}
+
+}
