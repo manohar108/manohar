2974e8f512e7c42abc0b582755b208f97341ec2c
manohar108 <chmanohar93@gmail.com>
2017-11-24 19:41:50 +0530

    hghg

--- src/main/java/fish/example2.java
+++ src/main/java/fish/example2.java
@@ -4,7 +4,8 @@ public class example2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+System.out.println("sample project");
+System.out.println("sample project");
 	}
 
 }
